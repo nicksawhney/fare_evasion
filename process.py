0bd677f44701036ef828e811e074fc5abe57e122
nicksawhney <nicksawhney@gmail.com>
2020-05-08 15:42:19 -0400

    improve code styling and quality

--- process.py
+++ process.py
@@ -1,4 +1,4 @@
-import pandas as pd 
+import pandas as pd
 import geopandas as gpd
 from matplotlib import pyplot as plt
 from mpl_toolkits.axes_grid1 import make_axes_locatable
@@ -7,15 +7,16 @@ import numpy as np
 import shapely
 
 '''
-Author: Nick Sawhney 
+Author: Nick Sawhney
 
-This is a set of utility functions I wrote during the process of exploring, analyzing, and visualizing Fare Evasion Arrests in New York City
+This is a set of utility functions I wrote during the process of exploring,
+analyzing, and visualizing Fare Evasion Arrests in New York City
 
 Documentation will become more detailed as the project expands
 '''
 
 
-def load_tos_arrests(hist_path='data/NYPD_Arrests_Data__Historic_.csv', 
+def load_tos_arrests(hist_path='data/NYPD_Arrests_Data__Historic_.csv',
                      ytd_path='data/NYPD_Arrest_Data__Year_to_Date_.csv',
                      save=False):
 
@@ -35,14 +36,14 @@ def load_tos_arrests(hist_path='data/NYPD_Arrests_Data__Historic_.csv',
 
 
 
-def load_all_data(station_file='data/stations.csv', 
+def load_all_data(station_file='data/stations.csv',
                   evasions_file='data/evasions.csv',
                   acs_file='data/2013_2017_acs.dta',
-                  tract_file = 'data/nyu_2451_34505',
+                  tract_file='data/nyu_2451_34505',
                   lines_file='data/Subway Lines.geojson',
-                  city_shapefile = 'data/new-york-city-boroughs.geojson',
+                  city_shapefile='data/new-york-city-boroughs.geojson',
                   include_arrests=True,
-                  include_race=True, 
+                  include_race=True,
                   start_date=pd.datetime(2010, 1, 1),
                   end_date=pd.datetime(2030, 1, 1)):
 
@@ -65,9 +66,9 @@ def load_all_data(station_file='data/stations.csv',
 
     # populate with fare evasion arrests if needed
     if include_arrests:
-        stations = populate_arrests(stations, 
-                                    evasions, 
-                                    start_date=start_date, 
+        stations = populate_arrests(stations,
+                                    evasions,
+                                    start_date=start_date,
                                     end_date=end_date,
                                     breakdown_race=include_race)
 
@@ -97,11 +98,12 @@ def load_all_data(station_file='data/stations.csv',
 def gpd_from_csv(filename, crs={'init':'epsg:4326'}, long='Longitude', lat='Latitude'):
     '''
         Helper function for load_all_data
-        Reads and returns a GeoDataFrame with geometry from "lat/long" columns (really can be any xy coordinates)
+        Reads and returns a GeoDataFrame with geometry from
+        "lat/long" columns (really can be any xy coordinates)
     '''
 
     file_pd = pd.read_csv(filename)
-    
+
     return gpd.GeoDataFrame(
         file_pd,
         geometry=gpd.points_from_xy(
@@ -111,35 +113,36 @@ def gpd_from_csv(filename, crs={'init':'epsg:4326'}, long='Longitude', lat='Lati
     )
 
 
-def populate_arrests(stations, 
-                     evasions, 
-                     start_date=pd.datetime(2010, 1, 1), 
-                     end_date=pd.datetime(2030, 1, 1), 
+def populate_arrests(stations,
+                     evasions,
+                     start_date=pd.datetime(2010, 1, 1),
+                     end_date=pd.datetime(2030, 1, 1),
                      arr_column='arrests',
                      breakdown_race=True,
                      combine_hispanic=True):
     '''
-        Populate the subway station DataFrame with the number of arrests at each station according to the evasions DataFrame
+        Populate the subway station DataFrame with the number of arrests
+        at each station according to the evasions DataFrame
         over a specified time period.
 
         stations: the DataFrame of subway stations
         evasions: the DataFrame of fare evasions
     '''
 
-    ev = evasions[evasions['ARREST_DATE'] >= start_date]
-    ev = ev[evasions['ARREST_DATE'] <= end_date]
-    
-    st = stations.copy()
+    ev_dates = evasions[evasions['ARREST_DATE'] >= start_date]
+    ev_dates = ev_dates[evasions['ARREST_DATE'] <= end_date]
+
+    st_copy = stations.copy()
 
     if arr_column in stations.columns:
-        st = st.drop(arr_column, axis=1)
+        st_copy = st_copy.drop(arr_column, axis=1)
 
-    if 'complex_id' in st.columns:
-        st = st.set_index('complex_id')
+    if 'complex_id' in st_copy.columns:
+        st_copy = st_copy.set_index('complex_id')
 
-    st[arr_column] = ev.groupby('station_id').count()['ARREST_DATE']
+    st_copy[arr_column] = ev_dates.groupby('station_id').count()['ARREST_DATE']
 
-    st[arr_column] = st[arr_column].fillna(value=0)
+    st_copy[arr_column] = st_copy[arr_column].fillna(value=0)
 
     if breakdown_race:
         station_race_groups = evasions.groupby(['station_id', 'PERP_RACE'])
@@ -155,7 +158,7 @@ def populate_arrests(stations,
                                     .set_index('station_id')      \
                                     .fillna(0)
 
-        
+
         race_name_dict = {
             'AMERICAN INDIAN/ALASKAN NATIVE': 'Native American',
             'ASIAN / PACIFIC ISLANDER': 'Asian',
@@ -167,34 +170,34 @@ def populate_arrests(stations,
 
         race_arrests_station = race_arrests_station.rename(race_name_dict, axis=1)
 
-        st[race_arrests_station.columns] = race_arrests_station
-        
+        st_copy[race_arrests_station.columns] = race_arrests_station
+
         if combine_hispanic:
             try:
-                st['Hispanic'] = st['Black Hispanic'] + st['White Hispanic'] 
+                st_copy['Hispanic'] = st_copy['Black Hispanic'] + st_copy['White Hispanic']
 
             except KeyError:
-                if 'White Hispanic' in st.columns:
-                    st['Hispanic'] = st['White Hispanic']
+                if 'White Hispanic' in st_copy.columns:
+                    st_copy['Hispanic'] = st_copy['White Hispanic']
 
-                elif 'Black Hispanic' in st.columns:
-                    st['Hispanic'] = st['Black Hispanic']
+                elif 'Black Hispanic' in st_copy.columns:
+                    st_copy['Hispanic'] = st_copy['Black Hispanic']
                 else:
-                    st['Hispanic'] = 0
+                    st_copy['Hispanic'] = 0
 
             finally:
-                st['White Hispanic'] = 0
-                st['Black Hispanic'] = 0
+                st_copy['White Hispanic'] = 0
+                st_copy['Black Hispanic'] = 0
 
-            st = st.drop(['Black Hispanic', 'White Hispanic'], axis=1)
+            st_copy = st_copy.drop(['Black Hispanic', 'White Hispanic'], axis=1)
 
-        st = st.fillna(0)
-    
-    return st
-    
+        st_copy = st_copy.fillna(0)
 
-def plot_num_arrests_station(stations, 
-                             lines, 
+    return st_copy
+
+
+def plot_num_arrests_station(stations,
+                             lines,
                              nyc,
                              census_data,
                              figsize=(25, 25),
@@ -212,34 +215,34 @@ def plot_num_arrests_station(stations,
 
     ax.set_ylim((40.55, 40.95))
     ax.set_xlim((-74.05, -73.70))
-    
+
     if plot_race:
-        
+
         divider = make_axes_locatable(ax)
 
         cax = divider.append_axes("top", size="5%", pad=0.1, )
-        
+
         cd = census_data \
             .dissolve(by='nta_name', aggfunc='mean')\
-            .plot(column='percentnonwhite', 
+            .plot(column='percentnonwhite',
                   ax=ax,
-#                   legend=True, 
+#                   legend=True,
                   alpha=0.5,
                   cax=cax,
                   legend_kwds={'label': "Percent Nonwhite",
                                'orientation': "horizontal"},
                   cmap='BuPu')
         cd.axis('off')
-        
-        
-        
+
+
+
     else:
         nyc.plot(ax=ax, alpha=0.2, color='purple')
 
     stations.plot(markersize=stations[arr_col]*mult, ax=ax, color='black')
     lines.plot(ax=ax, color='black')
-    
-    
+
+
 
     plt.axis('off')
 
@@ -255,7 +258,7 @@ def plot_num_arrests_station(stations,
 def get_intersecting_fips(station_buffer_row, census):
     '''
         Gets all census tracts within station_buffer_row
-        Applies a weight to each census tract based on the proportion of the buffer 
+        Applies a weight to each census tract based on the proportion of the buffer
         that is within the census tract
 
         Used in get_demo to provide weighted demographic information for each station.
@@ -263,38 +266,38 @@ def get_intersecting_fips(station_buffer_row, census):
         station_buffer: a list of station buffers to use for areas
         census: census tracts and their corresponding fips
     '''
-    
+
     intersections = []
-    
+
     st_buff = station_buffer_row['buff']
-    
+
     st_area = st_buff.area
-    
+
     if 'fips' in census.columns:
         census = census.set_index('fips')
-        
+
         # make sure the index is fips
-    
+
     for fips, tract in census.iterrows():
         census_geom = tract.geometry
-        
-        
+
+
         if st_buff.intersects(census_geom):
             # if the station buffer intersects with the census tract,
             # ad d the fips to the intersections and calculate its geometry
             # might as well add the weight too, if we can calculate it in the same function
-            
-            un = st_buff.intersection(census_geom)
-            weight = un.area / st_area
-            
-            intersections.append((fips, weight)) 
-    
+
+            buff_union = st_buff.intersection(census_geom)
+            weight = buff_union.area / st_area
+
+            intersections.append((fips, weight))
+
     return intersections
 
 
 def get_demo(station_row, census, indicators):
     '''
-        Get the weighted demographic information from the `indicator` column of `census` 
+        Get the weighted demographic information from the `indicator` column of `census`
         for station_row
 
         station.apply(get_demo, axis=1, args=(census, 'percentnonwhite'))
@@ -306,7 +309,7 @@ def get_demo(station_row, census, indicators):
     int_fips = get_intersecting_fips(station_row, census)
 
     vals = {}
-    
+
     for indicator in indicators:
         print(indicator, end='\r', flush=True)
         ind_val = 0
@@ -325,7 +328,7 @@ def split_arrest_data(evasions, cutoff=pd.datetime(2018, 2, 2), include_pre=Fals
         Useful for finding stats before and after a specific cutoff
     '''
 
-    if include_pre:    
+    if include_pre:
         pre_cutoff = evasions[evasions['ARREST_DATE'] <= cutoff]
         post_cutoff = evasions[evasions['ARREST_DATE'] > cutoff]
 
@@ -339,7 +342,7 @@ def split_arrest_data(evasions, cutoff=pd.datetime(2018, 2, 2), include_pre=Fals
 
 def get_buffer(stations, feet):
     '''
-        Creates a buffer of size feet (in feet) around each subway station, returns the 
+        Creates a buffer of size feet (in feet) around each subway station, returns the
         list of buffers (with the same index as the station)
     '''
     crs = stations.crs
@@ -351,7 +354,7 @@ def get_buffer(stations, feet):
 def get_station_arrests(evasions, stations, subway_radius):
     '''
         Find the station in which each arrest occured, provided the location of a station,
-        the location of an arrest, and a buffer around the station. 
+        the location of an arrest, and a buffer around the station.
 
         #TODO: remove spaghetti
         #TODO: do something with no_station currently unused
@@ -361,7 +364,7 @@ def get_station_arrests(evasions, stations, subway_radius):
     no_station = []
     count = 0
     num_evasions = len(evasions)
-    
+
     if not 'complex_id' in stations.columns:
         stations = stations.reset_index()
 
@@ -374,48 +377,45 @@ def get_station_arrests(evasions, stations, subway_radius):
         nonlocal count, num_evasions, station_arrests
 
         print(f'{round(count/num_evasions*100, ndigits=2)}% complete', end='\r')
-        
+
         geom = row.geometry
-        
+
         in_stations = []
-        
+
         for idx, buffer in enumerate(subway_radius):
             if buffer.contains(geom):
                 in_stations.append(idx)
 
-        if len(in_stations) == 0:
+        if not in_stations:
             no_station.append(row.ARREST_KEY)
-        
+
         elif len(in_stations) == 1:
             station_arrests[row.ARREST_KEY] = {
                 'name' : stations['complex_nm'].iloc[in_stations[0]],
                 'id' : stations['complex_id'].iloc[in_stations[0]]
             }
-            
-        elif len(in_stations) > 1:
+
+        else:
     #         print(in_stations)
             pts = np.array(
                 [geom.distance(stations.loc[station]['geometry']) for station in in_stations]
             )
     #         print(pts)
             nearest_idx = in_stations[pts.argmin()]
-            
+
     #         print(nearest_idx)
-            nm = stations.loc[nearest_idx]['complex_nm']
+            complex_nm = stations.loc[nearest_idx]['complex_nm']
 
             i_d = stations.loc[nearest_idx]['complex_id']
-        
+
             station_arrests[row.ARREST_KEY] = {
-                'name' : nm,
+                'name' : complex_nm,
                 'id' : i_d
             }
-                  
-        else:
-            print('something broke')
-            return
-        
+
+
         count += 1
-        
+
         return in_stations
 
     evasions.apply(get_nearest_station, axis=1)
@@ -427,8 +427,3 @@ def get_station_arrests(evasions, stations, subway_radius):
     evasion_arrests[['station_name', 'station_id']] = arr_id_stat
 
     return evasion_arrests
-
-
-
-
-
